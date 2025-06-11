619f5ce1a4b563c4970404228d19a24cab1468a5
JulettePeque <pequejulette012702@gmail.com>
2025-06-11 11:46:47 +0800

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <Router>
+      <Routes>
+        // Insert your routes here
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
